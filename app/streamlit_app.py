61110ae5e381a365845df4c85d359d1d4bc210c8
Charlie Rowe <cwr321@gmail.com>
2025-10-16 21:06:53 -0400

    updated streamlit cloud deployment

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -15,7 +15,7 @@ from utils import (
 ) 
 
 # Test CSV
-csv_path = os.path.join(os.path.dirname(__file__), "stock_buy_signals_ML.csv")
+csv_path = os.path.join(os.path.dirname(__file__), "..", "/outputs/stock_buy_signals_ML.csv")
 
 
 @st.cache_data(ttl=3600)
